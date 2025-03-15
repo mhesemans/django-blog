bb61cbd04b71884c5554768554402c0e6f0632af
Micha Hesemans <micha_hesemans@hotmail.com>
2025-03-15 06:24:35 +0000

    feat: added excerpt and updated on fields to model

--- blog/models.py
+++ blog/models.py
@@ -15,4 +15,5 @@ class Post(models.Model):
     content = models.TextField()
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
