586f768af7686949cd22879db8c17a6d49e1526c
Micha Hesemans <micha_hesemans@hotmail.com>
2025-03-23 05:01:54 +0000

    feat: updated title to not be unique

--- about/models.py
+++ about/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+
 
 # Create your models here.
 class About(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=200)
     content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
