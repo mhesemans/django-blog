b3144f9d828f93cf0396dffbdf71fd4c96a06b86
Micha Hesemans <micha_hesemans@hotmail.com>
2025-03-26 04:50:53 +0000

    Revert "feat: added cloudinary to about images"
    This reverts commit 433df5b1ab4f893b710f7bb75b417369abff98c7.

--- about/models.py
+++ about/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -7,7 +6,6 @@ from cloudinary.models import CloudinaryField
 class About(models.Model):
     title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
-    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
 
     def __str__(self):
